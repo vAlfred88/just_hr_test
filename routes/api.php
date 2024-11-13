3477e91c073a11b5fde71b33b405a91c55128f14
vlfred88 <valfred88@gmail.com>
2024-11-14 00:08:00 +0300

    Add api routes for movies

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::apiResource('movies', MovieController::class);
