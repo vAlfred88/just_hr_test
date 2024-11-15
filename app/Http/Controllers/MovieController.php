3277152741ef60ad6c5f3775b6ba8e6bc7b74d31
vlfred88 <valfred88@gmail.com>
2024-11-15 10:23:45 +0300

    Add resources for movies

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,44 +4,48 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreMovieRequest;
 use App\Http\Requests\UpdateMovieRequest;
+use App\Http\Resources\MovieCollection;
+use App\Http\Resources\MovieResource;
 use App\Models\Movie;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
     // Получение списка всех фильмов
-    public function index(): Collection
+    public function index(): MovieCollection
     {
-        return Movie::all();
+        return new MovieCollection(Movie::query()->paginate(1));
     }
 
     // Получение информации о конкретном фильме по ID
-    public function show($id)
+    public function show(Movie $movie): MovieResource
     {
-        return Movie::findOrFail($id);
+        return new MovieResource($movie);
     }
 
     // Создание нового фильма
-    public function store(StoreMovieRequest $request)
+    public function store(StoreMovieRequest $request): MovieResource
     {
-        return Movie::create($request->validated());
+        $validated = $request->validated();
+
+        $movie = Movie::query()->create($validated);
+
+        return new MovieResource($movie);
     }
 
     // Обновление информации о фильме
-    public function update(UpdateMovieRequest $request, $id)
+    public function update(UpdateMovieRequest $request, Movie $movie): MovieResource
     {
-        $movie = Movie::findOrFail($id);
+        $validated = $request->validated();
 
-        $movie->update($request->validated());
-        return $movie;
+        $movie->update($validated);
+
+        return new MovieResource($movie);
     }
 
     // Удаление фильма
-    public function destroy($id): JsonResponse
+    public function destroy(Movie $movie): JsonResponse
     {
-        $movie = Movie::findOrFail($id);
         $movie->delete();
 
         return response()->json(['message' => 'Movie deleted successfully']);
