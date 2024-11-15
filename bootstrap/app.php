8e0cfdc68cb12ce161de67b34e1fe04786cc35ee
vlfred88 <valfred88@gmail.com>
2024-11-15 10:25:07 +0300

    add exceptions for "not found"

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,17 +3,27 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $e) {
+            return response()->json([
+                'message' => 'Resource not found'
+            ], 404);
+        });
+
+        $exceptions->shouldRenderJsonWhen(function (Request $request, Throwable $e) {
+            return $request->expectsJson();
+        });
     })->create();
