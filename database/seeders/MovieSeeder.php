93b821aa67156b2052352088721ac980b92f41a1
vlfred88 <valfred88@gmail.com>
2024-11-15 10:24:16 +0300

    update database seeders

--- database/seeders/MovieSeeder.php
+++ database/seeders/MovieSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Movie;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class MovieSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Movie::factory()->count(1000)->create();
     }
 }
